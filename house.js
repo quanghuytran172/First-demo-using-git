c0c6be9cbbd6e6b8355e6a8046d994d822657f99
Quang Huy <quanghuytran172@gmail.com>
2020-03-24 21:18:22 +0700

    add prototype function

--- house.js
+++ house.js
@@ -1,5 +1,9 @@
 function House(){ //constructor function
 	this.floors = null;
 	this.furnitures = [];
+	this.isExist = true;
+}
+House.prototype.destroy = function(){
+	this.isExist = false;
 }
 module.exports = House;
